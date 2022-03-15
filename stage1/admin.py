0940ff6eba4f751f3978d0a662e5b994c58eb299
unknown <Admin@DESKTOP-8I9VRJD.std.strathmore.local>
2022-03-15 16:05:37 +0300

    creating models and superuser

--- stage1/admin.py
+++ stage1/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(OneThing)
+admin.site.register(ThingsToLearn)
+admin.site.register(ImproveHabits)
+admin.site.register(SocialLife)
+admin.site.register(LeisureActivity)
+admin.site.register(FamilyLife)
+admin.site.register(Career)
+admin.site.register(Qualities)
+admin.site.register(IdealFuture)
+admin.site.register(FutureToAvoid)
